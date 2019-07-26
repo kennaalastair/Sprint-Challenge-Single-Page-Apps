f81975ba600f50d0a4f1fc3c54b7f6636a57a1a9
Kenna <kennaalastair@gmail.com>
2019-07-26 11:05:02 -0700

    CharacterList displaying on route /characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,36 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard (props) {
+  const { character } = props;
+  const { created, gender, image, location, name, species, status } = props.character;
+  console.log(props);
+  console.log(name);
+  console.log(created);
+  console.log(gender);
+  console.log(location.name);
+  console.log(species);
+  console.log(status);
+  return (
+    <div className="character-wrap">
+      <div className="character-card">
+        <h1>{name}</h1>
+        <img src={image} />
+        <div className="character-created">
+          Created: <h3>{created}</h3>
+        </div>
+      <div className="character-gender">
+          Gender: <h3>{gender}</h3>
+        </div>
+        <div className="character-location">
+          Location: <h3>{location.name}</h3>
+        </div>
+        <div className="character-species">
+          Species: <h3>{species}</h3>
+        </div>
+        <div className="character-status">
+          status: <h3>{status}</h3>
+        </div>
+      </div>
+    </div>
+    // <span>todo: location</span>
+  )};
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
+    const getCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+          setCharacters(res.data.results);
+          console.log(res.data.results);
+        })
+        .catch(err => {
+          console.log("API isn't currently working, please try again later", err);
+        });
+    }
+    getCharacters();
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, [])
 
   return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>
+        {characters.map(character => (
+          <CharacterDetails key={character.id} character={character} />
+        ))}
+      </h2>
     </section>
 
+}
+
+function CharacterDetails({ character }) {
+  const { id, name, status, species, type } = character
+  return (
+    <Link to={`/characters/${id}`}>
+      <CharacterCard character={character} />
+    </Link>
+  );
 }
